1f59b06380588ba8bb6a290ac22a01a15cf69c95
Pritiranjan Biswal <pritiranjanbiswal46@gmail.com>
2026-08-28 23:56:06 +0530

    feat: update mail sender

--- server/utils/mailSender.js
+++ server/utils/mailSender.js
@@ -1,26 +1,33 @@
-const nodemailer=require("nodemailer")
+const nodemailer = require('nodemailer');
+require('dotenv').config();
 
+exports. sendEmail = async (email, title, body) => {
+  try{
+//2 step process
 
-const mailSender=async(email, title, body) => {
-    try{
-        let transporter=nodemailer.createTransport({
-            host:process.env.MAIL_HOST,
-            auth: {
-                user:process.env.MAIL_USER,
-                pass:process.env.MAIL_PASS
-            }
-        })
-        let info=await transporter.sendMail({
-            from:"StudyNotion || Kuna",
-            to:`${email}`,
-            subject:`${title}`,
-            html:`${body}`
-        })
-        console.log(info);
-        return info;
+//1:- create transporter
+const transporter = nodemailer.createTransport({
+    host:process.env.EMAIL_HOST,
+    secure:true,
+    auth:{
+        user:process.env.EMAIL_USER,
+        pass:process.env.EMAIL_PASS,
     }
-    catch(error) {
-        console.log(error.message);
-    }
-}
-module.exports=mailSender;
+});
+
+//2.send email
+let info = await transporter.sendMail({
+    from: 'StudyNotion || Phani Bhusan Mohanty',
+    to:`${email}`,
+    subject:`${title}`,
+    html:`${body}`,
+});
+
+console.log(info);
+return info;
+
+  }catch(err){
+    console.error('error occured while sending email :', err);
+    throw new Error('Failed to send email');
+  }
+} 
